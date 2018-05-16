cb7b76a3d6e2d8199f51f36d0b92ff11ee231d10
Nathan Bunch <31823043+taranoshi@users.noreply.github.com>
2018-05-16 17:53:07 -0400

    added qic

---
+++ Quantum Interface Classifier/doublePrimeX.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+#%matplotlib inline
+import numpy as np
+from scipy import linalg as la
+
+#import quantum computing fuctions
+from qiskit import QuantumProgram
+from qiskit.tools.visualization import plot_histogram, plot_state
+
+Q_Program = QuantumProgram()
+
+#initialize the registers
+qubit = Q_Program.create_quantum_register("qubit", 4)
+classic = Q_Program.create_classical_register("classic", 4)
+quantumCircuit = Q_Program.create_circuit("quantum_interface_classifier", [qubit], [classic])
+
+quantumCircuit.h(qubit[0])
+quantumCircuit.h(qubit[1])
+
+quantumCircuit.cu3(3.036,0,0, qubit[0], qubit[2])#check this
+
+quantumCircuit.x(qubit[0])
+quantumCircuit.ccx(qubit[0], qubit[1], qubit[2])
+quantumCircuit.x(qubit[1])
+quantumCircuit.ccx(qubit[0], qubit[1], qubit[2])
+
+quantumCircuit.u3(-0.6625,0,0, qubit[2])
+
+quantumCircuit.ccx(qubit[0], qubit[1], qubit[2])
+
+quantumCircuit.u3(-0.6625,0,0, qubit[2])
+
+quantumCircuit.cx(qubit[2], qubit[3])
+quantumCircuit.cx(qubit[3], qubit[2])
+quantumCircuit.cx(qubit[2], qubit[3])
+quantumCircuit.cx(qubit[1], qubit[2])
+
+quantumCircuit.h(qubit[0])
+quantumCircuit.measure(qubit[0], classic[0])
+quantumCircuit.measure(qubit[1], classic[1])
+
+result = Q_Program.execute(["quantum_interface_classifier"], backend='local_qasm_simulator', shots=1024)
+
+#return the result to console: bits and bit states that were counted
+print(result)
+print(result.get_data("quantum_interface_classifier"))
+
+#plot the counts, determine probability of each state
+qcounts = result.get_counts('quantum_interface_classifier')
+norm0000 = qcounts['0000'] / (qcounts['0000'] + qcounts['0010'])
+norm0010 = qcounts['0010'] / (qcounts['0000'] + qcounts['0010'])
+print("0000 Normalized: " + str(norm0000))
+print("0010 Normalized: " + str(norm0010))
+plot_histogram(result.get_counts('quantum_interface_classifier'))
